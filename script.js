d12369fc8c583d4956a2bf97b1eb3487b6720a6f
Muhamad Azka Firmansyah <116323822+Azka257@users.noreply.github.com>
2023-11-16 17:55:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+/*==================== toggle icon navbar ====================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+/*==================== scroll sections active link ====================*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+
+    /*==================== sticky navbar ====================*/
+    sections.forEach((sec) => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+    
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach((link) => {
+    
+            });
+        }
+    });
+    
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+
+
+};
+
+
+/*==================== scroll reveal ====================*/
+ScrollReveal({
+    //reset:true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+ScrollReveal().reveal('.home-content, .heading', {origin:'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {origin:'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img', {origin:'left'});
+ScrollReveal().reveal('.home-content p, .about-content', {origin:'right'});
+/*==================== typed js ====================*/
+// var typed = new Typed(".text", {
+//     strings: ["Programmer","Software Engginer","Marching Band"],
+//     typeSpeed: 100,
+//     typeSpeed: 100,
+//     backDelay: 1000,
+//     loop: true
+// })
